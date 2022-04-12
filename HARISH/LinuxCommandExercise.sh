737b573ca468178bb9bf94216e9710383ceed467
hk-axelor <103167218+hk-axelor@users.noreply.github.com>
2022-04-12 14:40:27 +0530

    Delete HARISH directory

--- HARISH/LinuxCommandExercise.sh
+++
@@ -1,7 +0,0 @@
-rep -l "@gmail.com" *.txt > EmailAddressFiles.txt
-cat EmailAddressfiles.txt
-grep -h "@gmail.com" *.txt > EmailAddresses.txt
-cat EmailAddresses.txt 
-sed 's/gmail/axelor/' EmailAddresses.txt
-
-Answer
