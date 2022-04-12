9af8af41eafe793182e10756ca40300926b1a7d6
hk-axelor <h.k@axelor.com>
2022-04-12 10:38:01 +0530

    Added Question.sh

---
+++ HARISH/Question.sh
@@ -0,0 +1,23 @@
+Fork this repo in your account (https://github.com/rch-axelor/Training)
+clone rch-axelor/Tranning (https://github.com/rch-axelor/Training) repo in your local 
+set origin (your account repo) & upstream (rch-axelor account repo) in your local 
+create a new branch wip-<your-first-name> (e.g. wip-riddhi) and checkout to the newly created branch.
+
+create new directory named <YOUR-FIRST-NAME> (e.g. RIDDHI).
+Add Linux command script file in this directory. (File name should be LinuxCommandExercise.sh).
+commit this file with specific message // message should be “Linux command exercise”.
+push in your local repo 
+create pull request(PR) to upstream repo project (rch-axelor/Training)
+
+Add the question I provided to create the script at the top in a multi-line comment block.  (In directly update that file)
+create one text file and list out all commands you execute till now in order to achieve this process.  
+create a patch file for those changes in the same directory. (without adding them )
+create a second patch file for the changes in the same directory. (after adding them)
+commit it with message & some description 
+push those this commit to the same PR created previously. 
+
+change last commit message.
+
+reset last commit in local & update the text file with next executed commands. 
+and commit those changes & push it to the same PR.  
+
